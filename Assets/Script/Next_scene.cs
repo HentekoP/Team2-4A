9294a6f879d5ee6a45c7452091517c7f51b24816
OoishiRen <ren-ooishi-20h@stu.kbc.ac.jp>
2022-02-14 10:14:48 +0900

    タイトル画面の追加

---
+++ Assets/Script/Next_scene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Next_scene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            if (SceneManager.GetActiveScene().name == "Title") {
+                SceneManager.LoadScene("game");
+            }
+            else if (SceneManager.GetActiveScene().name == "game")
+            {
+                SceneManager.LoadScene("Title");
+            }
+        }
+    }
+}
