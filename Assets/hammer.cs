d4d10f0e57be4f2432c3cac8b46b4da54d479264
DESKTOP-FH385H9\Noguc <yoshihiro-noguchi-20h@stu.kbc.ac.jp>
2022-02-15 09:22:34 +0900

    no message

---
+++ Assets/hammer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hammer : MonoBehaviour
+
+{
+    
+      void OnTriggerEnter(Collider other)
+    {
+        
+                gameObject.GetComponent<Rigidbody>();
+
+                
+                gameObject.isKinematic = false;
+               
+                
+            }
+        }
