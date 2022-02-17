fe2cd3ac2298958343b274afe881689984602f08
DESKTOP-FH385H9\Noguc <yoshihiro-noguchi-20h@stu.kbc.ac.jp>
2022-02-17 12:06:25 +0900

    破壊できた

--- Assets/Script/hammer.cs
+++ Assets/Script/hammer.cs
@@ -1,21 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class hammer : MonoBehaviour
 
+
 {
+    public SphereCollider bc;
     public Rigidbody rig;
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown("joystick button 2"))
+        {
+            Debug.Log("button2");
+            bc.enabled = true;
+        }
+        else
+        {
+            bc.enabled = false;
+        }
+    }
+
     void OnCollisionEnter(Collision collision)
     {
 
 
         rig = collision.gameObject.GetComponent<Rigidbody>();
         rig.isKinematic = false;
+        
 
         Debug.Log("Hit");
-       
-        
+
+
     }
+
 
 }
