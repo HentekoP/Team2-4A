3825f18b667dab170eac35da84dabfef2f0f6a06
KBC12006017\小浜大和 <towa-kohama-20h@stu.kbc.ac.jp>
2022-02-22 11:22:09 +0900

    no message

--- Assets/PlayerScript/createcube.cs
+++ Assets/PlayerScript/createcube.cs
@@ -24,12 +24,14 @@ public class createcube : MonoBehaviour
     }
     void CreateBlockObject()
     {
-        int row = 5;
-        int High = 5;
-        int column = 20;
-        float xoffset = 0.2f;
-        float yoffset = 0.1f;
-        float zoffset = 0.1f;
+        int row = 10;
+        int High = 10;
+        int column = 30;
+        float xoffset = 0.9f;
+        float yoffset = 0.3f;
+        float zoffset = 0.9f;
+        float yR = 0;
+        float count = 0;
 
         for(int h = 0; h < row; h++)
         {
@@ -37,25 +39,47 @@ public class createcube : MonoBehaviour
             {
                 for(int j = 0; j < High; j++)
                 {
-                    GameObject obj = Instantiate(prefabObj, Vector3.zero, Quaternion. identity);
-                    obj.transform.SetParent(parentTran);
-                    float xPos = xoffset * h;
-                    float yPos = yoffset * i;
-                    float zPos = zoffset * j;
-                    obj.transform.localPosition = new Vector3(xPos, yPos, zPos);
-
-                    int materialID = Random.Range(0, 3);
-                    Material mat = matCyan;
-                    switch (materialID)
+                    if (0 < h && h < row - 1 && 0 < j && j < High - 1) { }
+                    else
                     {
-                        case 1:
-                            mat = matBulue;
-                            break;
-                        case 2:
-                            mat = matYellow;
-                            break;
+                        GameObject obj = Instantiate(prefabObj, Vector3.zero, Quaternion.identity);
+                        if (0 < j && j < High - 1) {
+                            if (yR == 0 && count == 0)
+                            {
+                                yR = 90f;
+                                transform.Rotate(0, yR, 0, Space.World);
+                                count++;
+                            }
+                            else
+                            {
+                                yR = 0f;
+                                transform.Rotate(0, yR, 0, Space.World);
+                            }
+                        } else
+                        {
+                            
+                        }
+
+                        obj.transform.SetParent(parentTran);
+                        float xPos = xoffset * h;
+                        float yPos = yoffset * i;
+                        float zPos = zoffset * j;
+                        obj.transform.localPosition = new Vector3(xPos, yPos, zPos);
+
+                        int materialID = Random.Range(0, 3);
+                        Material mat = matCyan;
+                        switch (materialID)
+                        {
+                            case 1:
+                                mat = matBulue;
+                                break;
+                            case 2:
+                                mat = matYellow;
+                                break;
+                        }
+                        obj.GetComponent<MeshRenderer>().material = mat;
+
                     }
-                    obj.GetComponent<MeshRenderer>().material = mat;
                 }
             }
         }
