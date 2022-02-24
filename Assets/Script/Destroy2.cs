a91e81059ad350ae0f56798da44ea5222fd8160a
OoishiRen <ren-ooishi-20h@stu.kbc.ac.jp>
2022-02-24 14:06:33 +0900

    レンガの壁を作りました。

---
+++ Assets/Script/Destroy2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy2 : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        var rb = GetComponent<Rigidbody>();
+
+        //// 地面に衝突したら自オブジェクト削除
+        //if (collision.gameObject.name == "Plane")
+        //{
+        //    Destroy(gameObject, 1f);
+
+        //    // Playerと衝突したら、自オブジェクトとPlayerオブジェクトを削除
+        //}
+        //if (collision.gameObject.tag == "Block")
+        //{
+        //    rb.isKinematic = false;
+        //    // Playerと衝突したら、自オブジェクトとPlayerオブジェクトを削除
+        //}
+    }
+}
