62a56d657c5230442833b95058ea3ff71cca8b4b
4696neko <asuma-nakamura-20h@stu.kbc.ac.jp>
2022-03-11 12:52:23 +0900

    Break.csスクリプトをいじった

--- Assets/Script/Break.cs
+++ Assets/Script/Break.cs
@@ -4,29 +4,43 @@ using UnityEngine;
 
 public class Break : MonoBehaviour
 {
-    Vector3 ookisa;
-    public float life;
+    public int MAX = 150;
     void Start()
     {
-        ookisa = gameObject.transform.localScale;
-        life = 100f;
-    }
-
-    void Update()
-    {
-        
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        float collisionForce = collision.impulse.magnitude / Time.fixedDeltaTime;
-
-        if (collisionForce > 200.0f)
+        for (int i = 0; i < MAX; i++)
         {
-            ookisa.x = ookisa.x / 2;
-            ookisa.y = ookisa.y / 2;
-            ookisa.z = ookisa.z / 2;
-            gameObject.transform.localScale = ookisa;
+            var rand = Random.insideUnitSphere * 3;
+            var randColor = Random.ColorHSV();
+            var obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            obj.transform.position = rand;
+            obj.transform.localScale = Vector3.one * 0.3f;
+            obj.GetComponent<Renderer>().material.color = randColor;
         }
     }
+
+    //Vector3 ookisa;
+    //public float life;
+    //void Start()
+    //{
+    //    ookisa = gameObject.transform.localScale;
+    //    life = 100f;
+    //}
+
+    //void Update()
+    //{
+
+    //}
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    float collisionForce = collision.impulse.magnitude / Time.fixedDeltaTime;
+
+    //    if (collisionForce > 200.0f)
+    //    {
+    //        ookisa.x = ookisa.x / 2;
+    //        ookisa.y = ookisa.y / 2;
+    //        ookisa.z = ookisa.z / 2;
+    //        gameObject.transform.localScale = ookisa;
+    //    }
+    //}
 }
