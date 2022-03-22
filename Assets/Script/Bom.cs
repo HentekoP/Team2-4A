279833355cef1d837c0fcb8cc299824d25680ee9
4696neko <asuma-nakamura-20h@stu.kbc.ac.jp>
2022-03-22 15:09:40 +0900

    スクリプト追加　トリガーボタンが分からん

---
+++ Assets/Script/Bom.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bom : MonoBehaviour
+{
+    public ParticleSystem exp;
+    public GameObject obj;
+    public bool ExprosionFlg = false;
+
+    void Update()
+    {
+        if (/* && */Input.GetButtonDown("joystick button 12"))
+        {
+            ExprosionFlg = true;
+        }
+
+        if (ExprosionFlg == true)
+        {
+            Exprosion();
+            ExprosionFlg = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Hammer")
+        {
+            Exprosion();
+        }
+    }
+
+    void Exprosion()
+    {
+        exp.Play();
+        Destroy(obj);
+    }
+    //void Update()
+    //{
+    //    if (Input.GetButton("R2"))
+    //        Debug.Log("thisone");
+    //}
+}
