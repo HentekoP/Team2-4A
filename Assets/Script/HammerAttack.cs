47c6495467af5679c5f840c0a11886be8bc4d234
KBC12006064\kbc12006064 <ryuugo-higa-20h@stu.kbc.ac.jp>
2022-03-22 15:10:11 +0900

    なんとなくanimation

---
+++ Assets/Script/HammerAttack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerAttack : MonoBehaviour
+{
+    private CharacterController cCon;
+    private Vector3 velocity;
+    private Animator animator;
+    private bool hammer;
+    private bool hammerFlag = false;
+    [SerializeField] Animator AnimationImage = null;
+    private bool isHit;
+    void Start()
+    {
+        cCon = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("x"))
+        {
+            Debug.Log("アニメ再生してほしい。");
+            hammerFlag = true;
+            animator.SetBool("hammer", true);
+        }else{ 
+            animator.SetBool("hammer", false); 
+        }
+    }
+}
