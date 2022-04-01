b59400aaabbcc76f8ca7cd6fe2d4705288b38531
DESKTOP-FH385H9\Noguc <yoshihiro-noguchi-20h@stu.kbc.ac.jp>
2022-04-01 13:53:37 +0900

    RayCastの方向修正

--- Assets/Script/Scripts/HAKAI/Scripts/DemoController.cs
+++ Assets/Script/Scripts/HAKAI/Scripts/DemoController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [System.Runtime.InteropServices.Guid("AF60DA75-A174-48B8-B6FA-60D9127A836F")]
 public class DemoController : MonoBehaviour
 {
+    public GameObject ObA;
 
     // Start is called before the first frame update
     void Start()
@@ -15,20 +16,22 @@ public class DemoController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 direction = new Vector3(-90, 0, 0);
+        Ray _ray = new Ray(ObA.transform.position, direction);
         RaycastHit hit_info;
+      
 
-        if (Input.GetButton("joystick button 2"))
+        if (Input.GetButton("x"))
         {
 
             if (Physics.Raycast(_ray, out hit_info, 100, 1 << LayerMask.NameToLayer("Destructible"), QueryTriggerInteraction.Ignore))
             {
-                hit_info.collider.GetComponent<DestroyedPieceController>().cause_damage(_ray.direction * 0);
+                hit_info.collider.GetComponent<DestroyedPieceController>().cause_damage(_ray.direction * 150);
 
             }
 
         }
-        Debug.DrawRay(_ray.origin, _ray.direction * 10, Color.red, 5);
+        Debug.DrawRay(_ray.origin, _ray.direction * 1, Color.red, 5);
     }
 
   
