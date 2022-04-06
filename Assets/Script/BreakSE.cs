5c7efe3520162e774d3eaa1f5e1bf949cbe9f6fb
4696neko <asuma-nakamura-20h@stu.kbc.ac.jp>
2022-04-06 10:20:42 +0900

    SE調整中　動作不良起こすかも

--- Assets/Script/BreakSE.cs
+++ Assets/Script/BreakSE.cs
@@ -6,19 +6,29 @@ public class BreakSE : MonoBehaviour
 {
     public AudioClip sound1;
     public AudioClip sound2;
+    public AudioClip sound3;
+
     AudioSource audioSource;
+
+    public hamaaa hammerScript;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
     }
+
     void Update()
     {
         float tri = Input.GetAxis("RT");
 
-        if (tri == 1.0f && (GameObject.FindWithTag("bomb").activeInHierarchy == true))
+        if (tri == 1.0f && (GameObject.FindWithTag("bomb")/*.activeInHierarchy*/ == true))
         {
             audioSource.PlayOneShot(sound2);
+        }
+
+        if(hammerScript.objectHP >= 0)
+        {
+            audioSource.PlayOneShot(sound3);
         }
     }
 
