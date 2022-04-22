3a4144a160b43bad8807e4903ad37040e8b4cd95
OoishiRen <ren-ooishi-20h@stu.kbc.ac.jp>
2022-04-22 12:12:39 +0900

    ポーズメニューを作成中

--- Assets/Script/UI_Scripts/Pause.cs
+++ Assets/Script/UI_Scripts/Pause.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
-    public GameObject Pause_Panel,
-                      Pause_Cursor;
+    public GameObject Pause_Panel;
+    public RectTransform Pause_Cursor;
+
+    public int Menu_Num = 0;   //メニュー選択時の番号　0:ゲームに戻る 1:ステージ選択画面に戻る 2:ゲーム終了
+    bool Push_Flg = false; //連続入力防止用スイッチ
+
+    [SerializeField] private GameObject Pause_EndPanel; //エンド画面
+    public int Pause_EndNum = 0;   //メニュー選択時の番号　0:戻る 1:終了
+    public RectTransform Pause_EndCursor;
+    public bool End_flg = false;
 
     private void Start()
     {
         Pause_Panel.SetActive(false);
+        Pause_EndPanel.SetActive(false);
     }
 
     private void Update()
@@ -44,10 +54,154 @@ public class Pause : MonoBehaviour
     void Pause_Select()
     {
         var cursor_tr = Pause_Cursor.gameObject.GetComponent<Transform>();
+        //Lスティックの入力
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetAxis("Horizontal"))
+        if (!End_flg)
         {
+            //下入力した場合
+            if (v == 1)
+            {
+                if (Push_Flg == false)
+                {
+                    Push_Flg = true;
+                    Menu_Num++;
 
+                    //一番下より下入力をした場合
+                    if (Menu_Num >= 3)
+                    {
+                        Menu_Num = 0;   //メニュー番号を一番上に変更
+                        Pause_Cursor.position += new Vector3(0, 220, 0); //メニューの最初に移動
+                    }
+                    Pause_Cursor.position += new Vector3(0, -110, 0);   //カーソルを下に移動
+
+                    Debug.Log("P" + Menu_Num);
+                }
+            }
+            //上入力した場合
+            else if (v == -1)
+            {
+                if (Push_Flg == false)
+                {
+                    Push_Flg = true;
+                    Menu_Num--;
+
+                    //一番上より上入力をした場合
+                    if (Menu_Num <= -1)
+                    {
+                        Menu_Num = 2;   //メニュー番号を一番下に変更
+                        Pause_Cursor.position += new Vector3(0, -220, 0);   //メニューの最後に移動
+                    }
+                    Pause_Cursor.position += new Vector3(0, 110, 0);    //カーソルを上に移動
+
+                    Debug.Log("P" + Menu_Num);
+                }
+            }
+            //入力をリセット
+            else
+            {
+                Push_Flg = false;
+            }
+
+
+            if (Input.GetButtonDown("A"))
+            {
+                switch (Menu_Num)
+                {
+                    case 0:
+                        Pause_Panel.SetActive(!Pause_Panel.activeSelf);
+                        break;
+                    case 1:
+                        SceneManager.LoadSceneAsync("Menu");
+                        break;
+                    case 2:
+                        End_flg = true;
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+        }
+
+        else if (End_flg)
+        {
+            Pause_End();
+        }
+    }
+
+    void Pause_End()
+    {
+        Pause_EndPanel.SetActive(true);
+
+        //　ポーズUIが表示されてる時は停止
+        if (Pause_EndPanel.activeSelf)
+        {
+            End_flg = true;
+        }
+        if (Pause_EndPanel.activeSelf)
+        {
+            float v = Input.GetAxisRaw("Vertical");
+            float h = Input.GetAxisRaw("Horizontal");
+
+            //右入力した場合
+            if (h == 1)
+            {
+                if (Push_Flg == false)
+                {
+                    Push_Flg = true;
+                    Pause_EndNum--;
+
+                    //一番下より下入力をした場合
+                    if (Pause_EndNum <= -1)
+                    {
+                        Pause_EndNum = 1;   //メニュー番号を一番左に変更
+                        Pause_EndCursor.position += new Vector3(-780, 0, 0); //メニューの最初に移動
+                    }
+                    Pause_EndCursor.position += new Vector3(390, 0, 0);   //カーソルを下に移動
+
+                    Debug.Log("E" + Pause_EndNum);
+                }
+            }
+            //左入力した場合
+            else if (h == -1)
+            {
+                if (Push_Flg == false)
+                {
+                    Push_Flg = true;
+                    Pause_EndNum++;
+
+                    //一番上より上入力をした場合
+                    if (Pause_EndNum >= 2)
+                    {
+                        Pause_EndNum = 0;   //メニュー番号を一番右に変更
+                        Pause_EndCursor.position += new Vector3(780, 0, 0);   //メニューの最後に移動
+                    }
+                    Pause_EndCursor.position += new Vector3(-390, 0, 0);    //カーソルを上に移動
+
+                    Debug.Log("E" + Pause_EndNum);
+                }
+            }
+            //入力をリセット
+            else
+            {
+                Push_Flg = false;
+            }
+
+            if (Input.GetButtonDown("A"))
+            {
+                if (Pause_EndNum == 0)
+                {
+                    Pause_EndPanel.SetActive(false);
+                    End_flg = false;
+                }
+                else
+                {
+                    //UnityEditor.EditorApplication.isPlaying = false;  //デバッグ用
+                    Application.Quit();
+                }
+            }
         }
     }
 }
