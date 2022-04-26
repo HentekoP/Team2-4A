40c7ef369c87176fb9faae09936678b858fb3bb0
OoishiRen <ren-ooishi-20h@stu.kbc.ac.jp>
2022-04-26 15:00:47 +0900

    タイマースクリプト試作

---
+++ Assets/Script/UI_Scripts/Timer/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI Timer_txt;
+
+
+    void Start()
+    {
+        Timer_txt.text = "<sprite=0><sprite=0>:<sprite=0><sprite=0>";
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
