76c0ad97b839d573bf52b7886a233c7b0c42d790
4696neko <asuma-nakamura-20h@stu.kbc.ac.jp>
2022-04-27 15:01:43 +0900

    クリア判定の雛形作成

--- Assets/Script/Win.cs
+++ Assets/Script/Win.cs
@@ -1,13 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class Win : MonoBehaviour
-{
-    private int ChildCount;
+//public class Win : MonoBehaviour
+//{
+//    private int ChildCount;
 
-    void Start()
-    {
-        ChildCount = this.transform.childCount;
-    }
-}
+//    public int destroycounts;
+//    int clearcount;
+
+//    void Start()
+//    {
+//        ChildCount = this.transform.childCount;
+//        clearcount = ChildCount * destroycounts;
+//    }
+
+//    void Update()
+//    {
+//        if(clearcount == )
+//        {
+//            Debug.Log("クリア！");
+//        }
+//    }
+//}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+////
+////現在はすべて雛形とする
+////
+///////////////////////////////////////////////////////////////////////////////////////////
