77c59f1d9380d197f61d55f891fe9dadd0ce3f12
4696neko <asuma-nakamura-20h@stu.kbc.ac.jp>
2022-05-09 16:11:13 +0900

    アニメーション追加

--- Assets/Script/Win.cs
+++ Assets/Script/Win.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Win : MonoBehaviour
 {
@@ -9,19 +10,23 @@ public class Win : MonoBehaviour
 
     public float destroycounts;
     float clearcount;
+    public GameObject clear;
 
     void Start()
     {
         ChildCount = this.transform.childCount;
         clearcount = ChildCount * destroycounts/* * 4*/;
         Debug.Log(clearcount);
+        clear.SetActive(false);
     }
 
     void Update()
     {
         if (clearcount <= DestroyedPieceController.DestroyPieceCount)
         {
-            SceneManager.LoadScene("Result");
+            //SceneManager.LoadScene("Result");
+            clear.SetActive(true);
+
         }
     }
 }
