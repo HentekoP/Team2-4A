c99a0eb6a0c1c3b3f85a05661190cd0ec74b7d6f
KBC12006017\小浜大和 <towa-kohama-20h@stu.kbc.ac.jp>
2022-05-11 10:56:54 +0900

    スコア完成

--- Assets/Script/UI_Scripts/Result.cs
+++ Assets/Script/UI_Scripts/Result.cs
@@ -6,14 +6,36 @@ using UnityEngine.SceneManagement;
 
 public class Result : MonoBehaviour
 {
-    public int rank;
+    int rank;
+    int TotalScore;
+    int time;
     public Animator animator;
 
+    void Start()
+    {
+        rank = 0;
+    }
+
     void Update () {
-        
+        TotalScore = Score.GetTotalScore();
+        time = Timer.EndTime();
         //GetComponentを用いてAnimatorコンポーネントを取り出す.
         animator = GetComponent<Animator>();
  
+        if(TotalScore >= 10000)
+        {
+            rank = 1;
+        }else if (8000 <= TotalScore && TotalScore < 10000)
+        {
+            rank = 2;
+        }else if(4000 <= TotalScore && TotalScore < 8000)
+        {
+            rank = 3;
+        }else if(TotalScore < 4000)
+        {
+            rank = 4;
+        }
+
         //intパラメーターの値を設定する.
         animator.SetInteger("rank", rank);
 	}
