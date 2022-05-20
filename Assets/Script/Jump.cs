b372c90fc851af006fdbbb3f4f0fc03210cb3a05
4696neko <asuma-nakamura-20h@stu.kbc.ac.jp>
2022-05-20 11:01:11 +0900

    ジャンプ音追加

---
+++ Assets/Script/Jump.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    //[SerializeField] AudioClip[] se;
+    public AudioSource source;
+
+    void Awake()
+    {
+        // アタッチしたオーディオソースのうち1番目を使用する
+        source = GetComponent<AudioSource>()/*[0]*/;
+    }
+
+    void OnTriggerEnter(Collider hit)
+    {
+        if (/*hit.gameObject == other.gameObject && */Input.GetButton("Jump"))
+        {
+            source.Play()/*OneShot(se[0])*/;
+        }
+    }
+}
